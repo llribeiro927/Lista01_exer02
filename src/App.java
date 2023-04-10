18bf0e6af2385a09e6d49bbba85db283fe668dbf
llribeiro927 <lucasrlima@093gmail.com>
2023-04-10 00:19:18 -0300

    Primeiro

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Entradas: Um número 
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int numero = teclado.nextInt();
+        teclado.close();
+        // Processamento: Não tem 
+        // Sáida: O número informado foi (número) 
+       System.out.println("");
+    }
+}
